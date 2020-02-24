e42e30a62f3bc1ea7cbe928db6e78bc5fb845234
Barogthor <f.thery3103@gmail.com>
2020-02-24 21:20:20 +0100

    icons fix

--- src/main/java/org/myPlugin/VIcons.java
+++ src/main/java/org/myPlugin/VIcons.java
@@ -5,5 +5,5 @@ import com.intellij.openapi.util.IconLoader;
 import javax.swing.*;
 
 public class VIcons {
-    public static final Icon FILE = IconLoader.getIcon("/icons/jar-gray.png");
+    public static final Icon FILE = IconLoader.getIcon("/icons/v-logo_16.png");
 }
